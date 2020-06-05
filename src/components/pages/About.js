80c72832ac0ef216dee18687f9d321622e21fc54
Manoj <manoj.more@neosofttech.com>
2020-06-05 09:53:32 +0530

    React Project Deploy

---
+++ src/components/pages/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About page</h1>
+        <p className="lead">
+          The href attribute requires a valid value to be accessible. Provide a
+          valid, navigable address as the href value. If you cannot provide a
+          valid href, but still need the element to resemble a link, use a
+          button and change it with appropriate styles. Learn more
+        </p>
+
+        <p className="lead">
+          The href attribute requires a valid value to be accessible. Provide a
+          valid, navigable address as the href value. If you cannot provide a
+          valid href, but still need the element to resemble a link, use a
+          button and change it with appropriate styles. Learn more
+        </p>
+
+        <p className="lead">
+          The href attribute requires a valid value to be accessible. Provide a
+          valid, navigable address as the href value. If you cannot provide a
+          valid href, but still need the element to resemble a link, use a
+          button and change it with appropriate styles. Learn more
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
